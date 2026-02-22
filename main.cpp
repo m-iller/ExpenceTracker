206db3fc7d31dd8f70c34123406fbd1d541dafee
m-iller <melnikovmiron@gmail.com>
2026-02-22 20:40:14 +0300

    Fixed terminal usage

--- main.cpp
+++ main.cpp
@@ -2,22 +2,40 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]){
-    short int ctrlnum {1};
+string firstScrnMsg[2] = {
+    "Please select action:",
+    "0. Exit"
+};
+
+int main(){
+    short int ctrlnum {100};
+    int FSMsgSize {sizeof(firstScrnMsg)/sizeof(firstScrnMsg[0])};
 
     while (true) {
-        cout<<"Type 0 to exit:"<<endl;
-        cin>>ctrlnum;
-        cout<<ctrlnum<<endl; //neverending loop idk
+        for (int i {0}; i<FSMsgSize; i++){
+            cout<<firstScrnMsg[i]<<endl;
+        }
+
+        if (!(cin>>ctrlnum)){
+            cout<<"Invalid input..."<<endl;
+            cin.clear();
+            cin.ignore();
+
+            ctrlnum = 100;
+        }
 
         switch (ctrlnum)
         {
         case 0:
             return 0;
             break;
-        
+        case 100: //always continue; for error handling
+            break;
         default:
             break;
         }
+
+        cout<<"\x1b[2J\x1b[1;1H"; //ANSI code to clean up terminal
+        cout.flush();
     }
 }
